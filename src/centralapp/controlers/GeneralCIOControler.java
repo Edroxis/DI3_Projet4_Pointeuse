80d30601c2ce10886013bd37618fd482d0fcf415
Julien Amoros <ju.amoros@laposte.net>
2016-06-06 16:20:01 +0200

    ajout filtre par date

--- src/centralapp/controlers/GeneralCIOControler.java
+++ src/centralapp/controlers/GeneralCIOControler.java
@@ -2,6 +2,9 @@ package centralapp.controlers;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import centralapp.model.Employee;
 import centralapp.views.GeneralCIOView;
@@ -9,10 +12,14 @@ import centralapp.views.GeneralCIOView;
 public class GeneralCIOControler {
 	private CentralApp mainControler;
 	private GeneralCIOView view;
+	private String formatStr = "yyyy-MM-dd|HH:mm"+"-Europe/Paris";
+	private DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm-VV");
 	
 	public GeneralCIOControler(CentralApp mainControler) {
 		this.mainControler = mainControler;
 		view = new GeneralCIOView(mainControler, this);
+		view.setMinTextField(formatStr);
+		view.setMaxTextField(formatStr);
 	}
 	
 	public GeneralCIOView getView() {
@@ -21,5 +28,49 @@ public class GeneralCIOControler {
 	
 	public void updateTable(){
 		view.updateTable();
+	}
+	
+	public DateTimeFormatter getDateFormatter(){
+		return dateFormatter;
+	}
+	
+	public ZonedDateTime strToDate(String str){
+		ZonedDateTime result = null;
+		try{
+			result = ZonedDateTime.parse(str, dateFormatter);
+		}catch(DateTimeParseException e){
+			System.err.println("Invalid Date Format");
+		}
+		
+		return result;
+	}
+	
+	public class FilterEvent extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			//System.err.println("[GCIOC] Filter event");
+			String strDateMin = view.getMinTextField();
+			String strDateMax = view.getMaxTextField();
+			ZonedDateTime dateMin = strToDate(strDateMin);
+			ZonedDateTime dateMax = strToDate(strDateMax);
+			
+			view.setFilterMin(dateMin);
+			view.setFilterMax(dateMax);
+			
+			updateTable();
+		}
+	}
+	
+	public class ResetEvent extends MouseAdapter {
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			//System.err.println("[GCIOC] Reset event");
+			view.setFilterMin(null);
+			view.setFilterMax(null);
+			view.setMinTextField(formatStr);
+			view.setMaxTextField(formatStr);
+			
+			updateTable();
+		}
 	}
 }
--- src/centralapp/views/GeneralCIOView.java
+++ src/centralapp/views/GeneralCIOView.java
@@ -13,21 +13,25 @@ import javax.swing.JTable;
 import javax.swing.RowSorter;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
 import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import java.awt.ScrollPane;
 
 import javax.swing.BoxLayout;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
+@SuppressWarnings("serial")
 public class GeneralCIOView extends JPanel {
 
 	private CentralApp mainControler;
 	private GeneralCIOControler controler;
 	private JTable table;
 	private DefaultTableModel model;
-	private RowSorter<TableModel> sorter;
+	private JTextField dateMinTextField;
+	private JTextField dateMaxTextField;
+	
+	private ZonedDateTime filterMin;
+	private ZonedDateTime filterMax;
 
 	/**
 	 * Create the panel.
@@ -45,21 +49,77 @@ public class GeneralCIOView extends JPanel {
 		table = new JTable();
 		table.setAutoCreateRowSorter(true);
         scrollPane.setViewportView(table);
+        
+        JPanel panel = new JPanel();
+        add(panel);
+        
+        JLabel lblNewLabel = new JLabel("Print checks ");
+        panel.add(lblNewLabel);
+        
+        JLabel lblDatemin = new JLabel("after :");
+        panel.add(lblDatemin);
+        
+        dateMinTextField = new JTextField();
+        panel.add(dateMinTextField);
+        dateMinTextField.setColumns(17);
+        
+        JLabel lblDatemax = new JLabel("and before :");
+        panel.add(lblDatemax);
+        
+        dateMaxTextField = new JTextField();
+        panel.add(dateMaxTextField);
+        dateMaxTextField.setColumns(17);
+        
+        JButton btnFilter = new JButton("Filter");
+        panel.add(btnFilter);
+        btnFilter.addMouseListener(controler.new FilterEvent());
+        
+        JButton btnReset = new JButton("Reset");
+        panel.add(btnReset);
+        btnReset.addMouseListener(controler.new ResetEvent());
+	}
+	
+	public void setMinTextField(String str){
+		dateMinTextField.setText(str);
+	}
+	
+	public String getMinTextField(){
+		return dateMinTextField.getText();
+	}
+	
+	public void setMaxTextField(String str){
+		dateMaxTextField.setText(str);
+	}
+	
+	public String getMaxTextField(){
+		return dateMaxTextField.getText();
+	}
+	
+	public void setFilterMin(ZonedDateTime date){
+		filterMin = date;
+	}
+	
+	public void setFilterMax(ZonedDateTime date){
+		filterMax = date;
 	}
 
 	public void updateTable() {// update the model of the JTable
 		int i = 0;
 		String[][] data = new String[mainControler.getCompany().getTotalCIO()][5];
-		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd|HH:mm");
+		DateTimeFormatter dateFormatter = controler.getDateFormatter();
 
 		for (Employee emp : mainControler.getCompany().getEmployees()) {
 			for (CheckInOut cio : emp.getCheckInOut()) {
-				data[i][0] = Integer.toString(emp.getId());
-				data[i][1] = emp.getfName();
-				data[i][2] = emp.getlName();
-				data[i][3] = emp.getDpt().toString();
-				data[i][4] = cio.getDate().format(dateFormatter);
-				i++;
+				if((filterMin != null && cio.getDate().compareTo(filterMin)>=0) || filterMin == null){
+					if((filterMax != null && cio.getDate().compareTo(filterMax)<=0) || filterMax == null){
+						data[i][0] = Integer.toString(emp.getId());
+						data[i][1] = emp.getfName();
+						data[i][2] = emp.getlName();
+						data[i][3] = emp.getDpt().toString();
+						data[i][4] = cio.getDate().format(dateFormatter);
+						i++;
+					}
+				}
 			}
 		}
 		
