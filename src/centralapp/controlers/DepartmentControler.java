e6fa4713f3cfb2db38fdd160da2908ec351b5efe
Julien Amoros <ju.amoros@laposte.net>
2016-06-06 22:08:11 +0200

    Redaction javadoc

--- src/centralapp/controlers/DepartmentControler.java
+++ src/centralapp/controlers/DepartmentControler.java
@@ -15,36 +15,72 @@ import centralapp.model.ManagementDpt;
 import centralapp.model.Manager;
 import centralapp.views.DepartmentView;
 
+/**
+ * 
+ * 
+ * @author Julien
+ */
 public class DepartmentControler {
+	/**
+	 * Reference to the main Controler
+	 */
 	private CentralApp mainControler;
+
+	/**
+	 * Reference to the view
+	 */
 	private DepartmentView view;
-	
+
+	/**
+	 * @param mainControler
+	 *            Reference to the CentralApp
+	 */
 	public DepartmentControler(CentralApp mainControler) {
 		this.mainControler = mainControler;
 		view = new DepartmentView(mainControler, this);
 	}
-	
+
+	/**
+	 * @return Reference to the view
+	 */
 	public DepartmentView getView() {
 		return view;
 	}
-	
+
+	/**
+	 * Calls method to update view.
+	 * 
+	 * @param list
+	 *            List of Departments
+	 */
 	public void updateDepartmentsList(ArrayList<Department> list) {
 		view.updateDepartmentsList(list);
 	}
-	
+
+	/**
+	 * Calls method to update view.
+	 * 
+	 * @param list
+	 */
 	public void updatePeopleList(ArrayList<Employee> list) {
 		view.updatePeopleList(list);
 	}
-	
+
+	/**
+	 * Class to Manage Manager selection
+	 */
 	public class SelectEvent implements ItemListener {
 		@Override
 		public void itemStateChanged(ItemEvent event) {
-			if(event.getStateChange() == ItemEvent.SELECTED) {
+			if (event.getStateChange() == ItemEvent.SELECTED) {
 				view.updateFromChoosenDpt(view.getDpt());
 			}
 		}
 	}
-	
+
+	/**
+	 * Class to manage Add button
+	 */
 	public class AddEvent extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
@@ -52,28 +88,34 @@ public class DepartmentControler {
 			Company company = mainControler.getCompany();
 			Department newDpt = new Department(newDptName, company);
 			Employee futureManager = view.getManager();
-			if(futureManager != null) {
+			if (futureManager != null) {
 				newDpt.setManager(new Manager(futureManager));
 				futureManager.removeEmployee();
 			}
 			mainControler.notifyDptListModification();
 		}
 	}
-	
+
+	/**
+	 * Class to Manage Apply Button
+	 */
 	public class ApplyEvent extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
 			Department dpt = view.getDpt();
 			dpt.setName(view.getName());
 			Employee futureManager = view.getManager();
-			if(futureManager != null) {
+			if (futureManager != null) {
 				dpt.setManager(new Manager(futureManager));
 				futureManager.removeEmployee();
 			}
 			mainControler.notifyDptListModification();
 		}
 	}
-	
+
+	/**
+	 * Class to manage Remove Button
+	 */
 	public class RemoveEvent extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
--- src/centralapp/views/DepartmentView.java
+++ src/centralapp/views/DepartmentView.java
@@ -25,117 +25,172 @@ import centralapp.model.Employee;
 import centralapp.model.ManagementDpt;
 import centralapp.model.Manager;
 
+/**
+ * 
+ * @author Julien
+ */
 @SuppressWarnings("serial")
 public class DepartmentView extends JPanel {
+	/**
+	 * Reference to CentralApp
+	 */
 	private CentralApp mainControler;
+
+	/**
+	 * Reference to the Controler
+	 */
 	private DepartmentControler controler;
-	
+
+	/**
+	 * List of selectable Departments
+	 */
 	private JComboBox<Department> departmentsComboBox;
+
+	/**
+	 * TextField Containing Department Name
+	 */
 	private JTextField departmentNameField;
+
+	/**
+	 * Selectable List containing Employees
+	 */
 	private JComboBox<Employee> departmentManagerComboBox;
+
+	/**
+	 * Null Pointer
+	 */
 	private Employee nullPerson;
+
+	/**
+	 * Button to Remove Department
+	 */
 	private JButton departmentRemoveButton;
-	
+
+	/**
+	 * @param mainControler
+	 * @param dptControler
+	 */
 	public DepartmentView(CentralApp mainControler, DepartmentControler dptControler) {
 		this.mainControler = mainControler;
 		controler = dptControler;
-		
+
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		
+
 		departmentsComboBox = new JComboBox<Department>();
 		add(departmentsComboBox);
-		
-		//Create the form: name, button choose manager, button add, apply, button remove
+
+		// Create the form: name, button choose manager, button add, apply,
+		// button remove
 		JPanel departmentFormPanel = new JPanel();
 		add(departmentFormPanel);
-		FormLayout fl_departmentFormPanel = new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),
-				FormSpecs.RELATED_GAP_COLSPEC,},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				RowSpec.decode("default:grow"),});
+		FormLayout fl_departmentFormPanel = new FormLayout(
+				new ColumnSpec[] { FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+						FormSpecs.RELATED_GAP_COLSPEC, ColumnSpec.decode("default:grow"),
+						FormSpecs.RELATED_GAP_COLSPEC, },
+				new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, RowSpec.decode("default:grow"), });
 		departmentFormPanel.setLayout(fl_departmentFormPanel);
-		
+
 		JLabel departmentNameLabel = new JLabel("Name");
 		departmentFormPanel.add(departmentNameLabel, "2, 2, right, default");
-		
+
 		departmentNameField = new JTextField();
 		departmentFormPanel.add(departmentNameField, "4, 2, fill, default");
 		departmentNameField.setColumns(10);
-		
+
 		JLabel departmentManagerLabel = new JLabel("Manager");
 		departmentFormPanel.add(departmentManagerLabel, "2, 4");
-		
+
 		nullPerson = new Employee("none", "");
 		departmentManagerComboBox = new JComboBox<Employee>();
 		departmentFormPanel.add(departmentManagerComboBox, "4, 4");
-		
+
 		JPanel departmentButtonsPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
 		departmentFormPanel.add(departmentButtonsPanel, "4, 6, fill, fill");
-		
+
 		JButton departmentAddButton = new JButton("Add");
 		departmentButtonsPanel.add(departmentAddButton);
-		
+
 		JButton departmentApplyButton = new JButton("Apply");
 		departmentButtonsPanel.add(departmentApplyButton);
-		
+
 		departmentRemoveButton = new JButton("Remove");
 		departmentButtonsPanel.add(departmentRemoveButton);
-		
-		//Set up events
+
+		// Set up events
 		departmentsComboBox.addItemListener(controler.new SelectEvent());
 		departmentAddButton.addMouseListener(controler.new AddEvent());
 		departmentApplyButton.addMouseListener(controler.new ApplyEvent());
 		departmentRemoveButton.addMouseListener(controler.new RemoveEvent());
 	}
-	
+
+	/**
+	 * @return Department's name
+	 */
 	public String getName() {
 		return departmentNameField.getText();
 	}
-	
+
+	/**
+	 * @return reference to selected manager
+	 */
 	public Employee getManager() {
-		Employee manager = (Employee)departmentManagerComboBox.getSelectedItem();
-		if(manager == nullPerson)
+		Employee manager = (Employee) departmentManagerComboBox.getSelectedItem();
+		if (manager == nullPerson)
 			manager = null;
-			
+
 		return manager;
 	}
-	
+
+	/**
+	 * @return Reference to selected Department
+	 */
 	public Department getDpt() {
-		return (Department)departmentsComboBox.getSelectedItem();
+		return (Department) departmentsComboBox.getSelectedItem();
 	}
-	
+
+	/**
+	 * Update Dpt selectable list
+	 * 
+	 * @param dptsList
+	 *            the department list
+	 */
 	public void updateDepartmentsList(ArrayList<Department> dptsList) {
 		Department selectedDpt = getDpt();
-		
+
 		departmentsComboBox.removeAllItems();
-		for(Department dpt : dptsList)
+		for (Department dpt : dptsList)
 			departmentsComboBox.addItem(dpt);
-		
+
 		departmentsComboBox.setSelectedItem(selectedDpt);
 	}
-	
+
+	/**
+	 * Update PeopleList
+	 * 
+	 * @param list
+	 *            Employee list
+	 */
 	public void updatePeopleList(ArrayList<Employee> list) {
 		Employee selectedManager = getManager();
-		
+
 		departmentManagerComboBox.removeAllItems();
 		departmentManagerComboBox.addItem(nullPerson);
-		for(Employee emp : list)
+		for (Employee emp : list)
 			departmentManagerComboBox.addItem(emp);
-		
+
 		departmentManagerComboBox.setSelectedItem(selectedManager);
 	}
-	
-	public void updateFromChoosenDpt(Department dpt) {		
+
+	/**
+	 * Update Peaople List Depending on choosen dpt
+	 * 
+	 * @param dpt
+	 *            the Department
+	 */
+	public void updateFromChoosenDpt(Department dpt) {
 		departmentNameField.setText(dpt.toString());
-		if(dpt.getManager() == null)
+		if (dpt.getManager() == null)
 			departmentManagerComboBox.setSelectedItem(nullPerson);
 		else
 			departmentManagerComboBox.setSelectedItem(dpt.getManager());
